f1335dd7a3ff64352b33596c56f9c94dc9d5ab4a
DPChanny <kyoungchan0516@gmail.com>
2026-03-15 13:41:14 +0900

    config update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [preact()],
-  server: {
-    port: 8080,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [preact(), tsconfigPaths()],
 });
