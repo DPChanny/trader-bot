14ccede52fa9599cbb44bb2b5b5ca368ef405de8
DPChanny <kyoungchan0516@gmail.com>
2026-04-14 16:38:39 +0900

    adjustment

--- python/bot/routers/on_ready_router.py
+++ python/bot/routers/on_ready_router.py
@@ -10,19 +10,9 @@ from ..services import on_ready_service
 
 def include_on_ready_router(bot: commands.Bot) -> None:
     on_ready_lock = asyncio.Lock()
-    on_ready_completed = False
 
     @bot.event
     @bot_router
     async def on_ready(session: AsyncSession):
-        nonlocal on_ready_completed
-
-        if on_ready_completed:
-            return
-
         async with on_ready_lock:
-            if on_ready_completed:
-                return
-
             await on_ready_service(bot, session)
-            on_ready_completed = True
