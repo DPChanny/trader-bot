475efddb7d2bea30c00da56a0f0830edd4a3e3be
DPChanny <kyoungchan0516@gmail.com>
2026-04-20 03:34:56 +0900

    bug fix

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -1,13 +1,13 @@
 export const apps = [
   {
-    name: "trader-bot-backend",
+    name: "trader-bot-prod-pm2-backend",
     cwd: "/var/www/trader-bot/python",
     script: "./.venv/bin/python",
-    args: "-m uvicorn backend.main:app --host localhost --port 8000",
+    args: "-m uvicorn backend.main:app --host 127.0.0.1 --port 8000",
     kill_timeout: 3000,
   },
   {
-    name: "trader-bot-bot",
+    name: "trader-bot-prod-pm2-bot",
     cwd: "/var/www/trader-bot/python",
     script: "./.venv/bin/python",
     args: "-m bot.main",
