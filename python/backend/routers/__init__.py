1f1c5f665b6cdf0e0b19002b0301f905e454c7b9
DPChanny <kyoungchan0516@gmail.com>
2026-04-21 12:00:32 +0900

    settings

--- python/backend/routers/__init__.py
+++ python/backend/routers/__init__.py
@@ -1,12 +1,24 @@
-from .auction_router import auction_router as auction_router
-from .auth_router import auth_router as auth_router
-from .guild_router import guild_router as guild_router
-from .member_router import member_router as member_router
-from .position_router import position_router as position_router
-from .preset_member_position_router import (
-    preset_member_position_router as preset_member_position_router,
-)
-from .preset_member_router import preset_member_router as preset_member_router
-from .preset_router import preset_router as preset_router
-from .tier_router import tier_router as tier_router
-from .user_router import user_router as user_router
+from .auction_router import auction_router
+from .auth_router import auth_router
+from .guild_router import guild_router
+from .member_router import member_router
+from .position_router import position_router
+from .preset_member_position_router import preset_member_position_router
+from .preset_member_router import preset_member_router
+from .preset_router import preset_router
+from .tier_router import tier_router
+from .user_router import user_router
+
+
+__all__ = [
+    "auction_router",
+    "auth_router",
+    "guild_router",
+    "member_router",
+    "position_router",
+    "preset_member_position_router",
+    "preset_member_router",
+    "preset_router",
+    "tier_router",
+    "user_router",
+]
