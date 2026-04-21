b35110953f4c11f2c28783c01b1c89932b0381d1
DPChanny <kyoungchan0516@gmail.com>
2026-04-22 04:26:02 +0900

    cloudwatch fix

--- infra/cloudwatch/ami.sh
+++ infra/cloudwatch/ami.sh
@@ -11,6 +11,7 @@ sudo dpkg -i "${TMP_DEB}" || sudo apt-get -f install -y
 rm -f "${TMP_DEB}"
 
 sudo install -d -m 755 /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d
+sudo rm -f /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d/* || true
 
 sudo systemctl disable amazon-cloudwatch-agent
 sudo systemctl stop amazon-cloudwatch-agent || true
