06b7521e942fb2d9e4f0166d8666829906aa373a
DPChanny <kyoungchan0516@gmail.com>
2026-04-22 03:58:12 +0900

    no default

--- infra/nginx/ami.sh
+++ infra/nginx/ami.sh
@@ -7,10 +7,10 @@ cd /
 
 sudo apt-get install -y --no-install-recommends nginx
 
-sudo rm -f /etc/nginx/sites-available/* || true
-sudo rm -f /etc/nginx/sites-enabled/* || true
-
 sudo nginx -t
 
 sudo systemctl disable nginx
 sudo systemctl stop nginx || true
+
+sudo rm -f /etc/nginx/sites-available/* || true
+sudo rm -f /etc/nginx/sites-enabled/* || true
