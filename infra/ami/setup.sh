31edd11bfe608dfc659456d9e431deef415ef013
DPChanny <kyoungchan0516@gmail.com>
2026-04-22 02:51:30 +0900

    fix

--- infra/ami/setup.sh
+++ infra/ami/setup.sh
@@ -7,17 +7,8 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
 	curl \
 	git \
-	jq \
 	lsof \
-	unzip \
 	htop
-
-TMP_DIR="$(mktemp -d)"
-trap 'rm -rf "${TMP_DIR}"' EXIT
-
-curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "${TMP_DIR}/awscliv2.zip"
-unzip -q "${TMP_DIR}/awscliv2.zip" -d "${TMP_DIR}"
-sudo "${TMP_DIR}/aws/install" --update
 
 sudo install -d -m 755 -o ubuntu -g ubuntu /var/www
 sudo rm -rf /var/www/trader-bot
