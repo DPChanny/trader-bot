14f2bc50d023642d85244d6d5a61e032e33a6c70
DPChanny <kyoungchan0516@gmail.com>
2026-04-22 15:16:44 +0900

    npm by pm2

--- infra/ami/python/setup.sh
+++ infra/ami/python/setup.sh
@@ -7,10 +7,6 @@ cd /
 
 bash /var/www/trader-bot/infra/ami/setup.sh
 
-curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
-sudo apt-get install -y --no-install-recommends nodejs
-sudo npm install -g npm@latest
-
 curl -fsSL https://astral.sh/uv/install.sh | sudo env UV_INSTALL_DIR=/usr/local/bin sh
 
 if [ -d "/var/www/trader-bot/python" ]; then
--- infra/pm2/ami.sh
+++ infra/pm2/ami.sh
@@ -5,6 +5,10 @@ export DEBIAN_FRONTEND=noninteractive
 
 cd /
 
+curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
+sudo apt-get install -y --no-install-recommends nodejs
+sudo npm install -g npm@latest
+
 sudo npm install -g pm2
 command -v pm2 >/dev/null
 
