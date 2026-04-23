d8b46b49503bbd9fe57872da071449bf4fde361c
DPChanny <kyoungchan0516@gmail.com>
2026-04-23 19:19:16 +0900

    setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [TanStackRouterVite(), react()],
+    plugins: [tanstackRouter(), react()],
     resolve: {
       tsconfigPaths: true,
     },
